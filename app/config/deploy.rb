ed1471d0653203b78da65b6babad7e87195ff44d
Marc MacLeod <marbemac@gmail.com>
2011-06-14 17:03:46 -0400

    small
    Signed-off-by: Marc MacLeod <marbemac@gmail.com>

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -8,8 +8,8 @@ set :deploy_via,  :rsync_with_remote_cache
 set :scm,         :git
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `subversion`, `mercurial`, `perforce`, `subversion` or `none`
 
-set :shared_files,      ["app/config/parameters.ini"]
-set :shared_children,     [app_path + "/logs", web_path + "/uploads", "vendor"]
+#set :shared_files,      ["app/config/parameters.ini"]
+set :shared_children,     [app_path + "/logs", web_path + "/uploads"]
 
 role :web,        domain                         # Your HTTP server, Apache/etc
 role :app,        domain                         # This may be the same as your `Web` server
@@ -18,18 +18,3 @@ role :db,         domain, :primary => true       # This is where Rails migration
 set :dump_assetic_assets, true
 set   :use_sudo,          true
 set  :keep_releases,      3
-
-namespace :symfony do
-  desc "Update the vendor libraries"
-  task :update_vendors do
-    run "cd #{latest_release} && #{php_bin} bin/vendors.php"
-  end
-end
-
-before "deploy:finalize_update" do
-  # share the children first (to get the vendor symlink)
-  deploy.share_childs
-
-  # update the vendors
-  symfony.update_vendors
-end

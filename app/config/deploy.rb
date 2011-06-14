1f1bc1fc9dbc29f91bb6382eb0ad632218cff08d
Marc MacLeod <marbemac@gmail.com>
2011-06-14 17:20:59 -0400

    update
    Signed-off-by: Marc MacLeod <marbemac@gmail.com>

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -3,33 +3,35 @@ set :application, "The Whoot"
 set :domain,      "thewhoot.com"
 set :deploy_to,   "/srv/www/whoot"
 
-set :repository,  "https://marbemac@github.com/whoot/whoot.git"
+#set :repository,  "https://marbemac@github.com/whoot/whoot.git"
+#set :scm,         :git
+set :repository,  "file:///srv/www/whoot"
+set :scm,         :none
 set :deploy_via,  :rsync_with_remote_cache
-set :scm,         :git
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `subversion`, `mercurial`, `perforce`, `subversion` or `none`
 
-set :shared_files,      ["app/config/parameters.ini"]
-set :shared_children,     [app_path + "/logs", web_path + "/uploads", "vendor"]
+#set :shared_files,      ["app/config/parameters.ini"]
+set :shared_children,     [app_path + "/logs", web_path + "/uploads"]
 
 role :web,        domain                         # Your HTTP server, Apache/etc
 role :app,        domain                         # This may be the same as your `Web` server
 role :db,         domain, :primary => true       # This is where Rails migrations will run
 
-set :dump_assetic_assets, true
-set   :use_sudo,          true
-set  :keep_releases,      3
+set  :dump_assetic_assets, true
+set  :use_sudo,            true
+set  :keep_releases,       3
 
-namespace :symfony do
-  desc "Update the vendor libraries"
-  task :update_vendors do
-    run "cd #{latest_release} && #{php_bin} bin/vendors install"
-  end
-end
+#namespace :symfony do
+#  desc "Update the vendor libraries"
+#  task :update_vendors do
+#    run "cd #{latest_release} && #{php_bin} bin/vendors install"
+#  end
+#end
 
-before "deploy:finalize_update" do
-  # share the children first (to get the vendor symlink)
-  deploy.share_childs
+#before "deploy:finalize_update" do
+#  # share the children first (to get the vendor symlink)
+#  deploy.share_childs
 
-  # update the vendors
-  symfony.update_vendors
-end
+#  # update the vendors
+#  symfony.update_vendors
+#end
